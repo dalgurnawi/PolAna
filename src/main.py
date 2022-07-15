a9211366ef6994a2282022cfd4557a6489077479
Krzysztof Buzar <kbuzar@gmail.com>
2022-07-15 12:57:58 +0200

    Scrapper updated

--- src/main.py
+++ src/main.py
@@ -2,10 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 import numpy as np
 import re
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import pandas as pd
+import time
 
 # clean = lambda s: str(re.sub('[\W_]+', ' ', s))
 
-
+# THIS WORKS GREAT
 def get_text(article_url):
     res = requests.get(article_url)
     html_page = res.content
@@ -15,9 +23,28 @@ def get_text(article_url):
     text = ' '.join([s.text for s in article[0].findAll('p')])
     return text
 
-
 text = get_text("https://www.washingtonpost.com/national-security/2022/05/21/russia-ukraine-victory/")
 print(text)
+
+
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+# driver.get("https://www.google.com")
+
+# driver = webdriver.Chrome("C:/chromedriver.exe")
+# driver.get('https://adfontesmedia.com/interactive-media-bias-chart/')
+# time.sleep(200)
+# points = driver.find_element(By.CLASS_NAME, "toolTipQuality")
+# print(points)
+
+# res = requests.get("https://adfontesmedia.com/interactive-media-bias-chart/")
+# html_page = res.content
+# soup = BeautifulSoup(html_page, 'html.parser')
+# print(soup)
+# article = soup.body.findAll('svg-tooltip toolTipArticle')
+# print(article)
+# # text = ' '.join([clean(s.text) for s in article[0].findAll('p')])
+# text = ' '.join([s.text for s in article[0].findAll('p')])
+# print(text)
 
 # list_of_articles = article_urls(blog_address)
 # print(list_of_articles)
